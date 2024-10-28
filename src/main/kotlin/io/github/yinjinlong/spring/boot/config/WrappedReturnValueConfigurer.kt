cbc370c9a94ed4823d9165418069cfd613279d39
YJL <yjl_1567@qq.com>
2024-10-28 16:34:50 +0800

    修复返回值处理

--- src/main/kotlin/io/github/yinjinlong/spring/boot/config/WrappedReturnValueConfigurer.kt
+++ src/main/kotlin/io/github/yinjinlong/spring/boot/config/WrappedReturnValueConfigurer.kt
@@ -3,11 +3,11 @@ package io.github.yinjinlong.spring.boot.config
 import io.github.yinjinlong.spring.boot.response.JsonResponse
 import io.github.yinjinlong.spring.boot.support.MessageConverter
 import io.github.yinjinlong.spring.boot.support.ReturnValueHandler
+import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
 
@@ -24,10 +24,15 @@ class WrappedReturnValueConfigurer : WebMvcConfigurationSupport() {
     @Autowired
     lateinit var ctx: ApplicationContext
 
-    override fun createRequestMappingHandlerAdapter() = RequestMappingHandlerAdapter().apply {
-        returnValueHandlers = arrayListOf<HandlerMethodReturnValueHandler>(
+    @PostConstruct
+    fun init() {
+        returnValueHandlers.add(
             ReturnValueHandler(ctx.getBeansOfType(MessageConverter::class.java).values.toList())
         )
         JsonResponse.registerFactory(templateProperties.responseJsonFactory)
+    }
+
+    override fun createRequestMappingHandlerAdapter() = RequestMappingHandlerAdapter().apply {
+        returnValueHandlers = this@WrappedReturnValueConfigurer.returnValueHandlers
     }
 }
