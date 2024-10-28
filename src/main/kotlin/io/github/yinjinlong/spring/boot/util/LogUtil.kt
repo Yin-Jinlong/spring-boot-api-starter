8ae19e2ddb9d387ca470441cb396f31dff50c349
YJL <yjl_1567@qq.com>
2024-10-28 20:00:45 +0800

    明确Logger类型

--- src/main/kotlin/io/github/yinjinlong/spring/boot/util/LogUtil.kt
+++ src/main/kotlin/io/github/yinjinlong/spring/boot/util/LogUtil.kt
@@ -2,4 +2,4 @@ package io.github.yinjinlong.spring.boot.util
 
 import java.util.logging.Logger
 
-fun Any.getLogger() = Logger.getLogger(this.javaClass.toGenericString())
+fun Any.getLogger(): Logger = Logger.getLogger(this.javaClass.toGenericString())
