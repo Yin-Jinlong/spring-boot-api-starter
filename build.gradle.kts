e6fd3017f9af47f3636b12fbc1bcc4209525491e
YJL <yjl_1567@qq.com>
2024-11-03 17:54:01 +0800

    添加Interceptors和ArgumentResolvers

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "io.github.yin-jinlong"
-version = "0.2.0"
+version = "0.2.1"
 description = "SpringBoot Kotlin Api服务 Starter"
 
 Props.init(rootDir)
--- src/main/kotlin/io/github/yinjinlong/spring/boot/config/WrappedReturnValueConfigurer.kt
+++ src/main/kotlin/io/github/yinjinlong/spring/boot/config/WrappedReturnValueConfigurer.kt
@@ -9,11 +9,16 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.StringHttpMessageConverter
+import org.springframework.web.context.request.WebRequestInterceptor
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler
 import org.springframework.web.servlet.HandlerExceptionResolver
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
 import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver
+import kotlin.reflect.KClass
 
 /**
  * @author YJL
@@ -29,11 +34,39 @@ class WrappedReturnValueConfigurer(
         JsonResponse.registerFactory(templateProperties.responseJsonFactory)
     }
 
+    fun <T : Any> getBeans(type: KClass<T>): Collection<T> {
+        return runCatching {
+            applicationContext!!.getBeansOfType(type.java).values
+        }.getOrDefault(emptyList())
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        runCatching {
+            getBeans(HandlerInterceptor::class).forEach {
+                registry.addInterceptor(it)
+            }
+            getBeans(WebRequestInterceptor::class).forEach {
+                registry.addWebRequestInterceptor(it)
+            }
+        }
+    }
+
+    override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver>) {
+        val set = LinkedHashSet<HandlerMethodArgumentResolver>()
+        set.addAll(argumentResolvers)
+        runCatching {
+            set.addAll(
+                getBeans(HandlerMethodArgumentResolver::class)
+            )
+        }
+        argumentResolvers.clear()
+        argumentResolvers.addAll(set)
+    }
+
     override fun addReturnValueHandlers(returnValueHandlers: MutableList<HandlerMethodReturnValueHandler>) {
         returnValueHandlers.add(
             ReturnValueHandler(
-                applicationContext!!.getBeansOfType(MessageConverter::class.java)
-                    .values
+                getBeans(MessageConverter::class)
             )
         )
     }
